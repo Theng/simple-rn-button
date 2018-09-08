21465267329d4fb99c9e54d3392bf11d0e09e69d
Theng <thengvlog@gmail.com>
2018-09-08 10:43:55 +0700

    Init lib

---
+++ index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+export default class RNSimpleRnButton extends Component {
+ 
+  render() {
+    return (
+      <View>
+          <Text>Simple Button Test</Text>
+      </View>
+    );
+  }
+}
+
+export default RNSimpleRnButton;
