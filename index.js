6e99e5f15acea111a6a88cc00680c6799cc75662
Theng <thengvlog@gmail.com>
2018-09-15 09:28:11 +0700

    props

--- index.js
+++ index.js
@@ -4,16 +4,41 @@ import {
   Text,
   TouchableOpacity
 } from 'react-native';
+import PropTypes from 'prop-types'; 
 export default class RNSimpleRnButton extends Component {
  
   render() {
     return (
-      <TouchableOpacity onPress={this.props.onClick()}>
-        <View>
-            <Text>Simple Button Test</Text>
+      <TouchableOpacity  hitSlop={{ top: this.props.hitSlop, left: this.props.hitSlop, bottom: this.props.hitSlop, right: this.props.hitSlop }} onPress={()=>this.props.onClick()}>
+        <View  style={[styles.btnContainer,{backgroundColor:this.props.backgroundColor}]}>
+            <Text style={[styles.textStyle,{color:this.props.textColor}]}>{this.props.title}</Text>
         </View>
       </TouchableOpacity>
     );
   }
 }
 
+RNSimpleRnButton.propTypes = {
+  title:PropTypes.string,
+  hitSlop:PropTypes.number,
+  onClick:PropTypes.func.isRequired,
+  textColor:PropTypes.string,
+  backgroundColor:PropTypes.string,
+}
+RNSimpleRnButton.defaultProps = {
+  title: 'Title',
+  hitSlop:0,
+  textColor:"white",
+  backgroundColor:"#2ecc71"
+};
+
+
+const styles={
+  btnContainer:{
+    padding:16,
+    borderRadius:7
+  },
+  textStyle:{
+    textAlign:"center"
+  }
+}
